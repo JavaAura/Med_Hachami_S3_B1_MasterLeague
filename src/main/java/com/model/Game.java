7a25e9adcbeede411ca185114834d0fac27b855f
MedHachami <moha.hach.9@gmail.com>
2024-10-23 17:51:29 +0100

    join column with tournament

--- src/main/java/com/model/Game.java
+++ src/main/java/com/model/Game.java
@@ -1,13 +1,21 @@
 package com.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
+@Table(name = "game")
 public class Game {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +24,21 @@ public class Game {
     @NotNull
     @Size(min = 3, max = 75)
     private String name;
+
+
+     @Column(name = "difficulty", nullable = false)
+    @Min(value = 1, message = "Difficulty must be at least 1")
+    @Max(value = 6, message = "Difficulty cannot exceed 6")
+    private int difficulty;
+
+    @Column(name = "duration_average_match", nullable = false)
+    @Min(value = 1, message = "Average match duration must be at least 1 minute")
+    @Max(value = 180, message = "Average match duration cannot exceed 180 minutes")
+    private int durationAverageMatch;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tournament_id", nullable = false)
+    private Tournament tournament;
 
 
 
